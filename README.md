c841589950add986ca14bfb696c1a409e9cfe72c
Babji8800 <sviatoslav701@gmail.com>
2023-04-08 17:48:19 +0300

    Auto deploy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vr-train
+
+Updated
---
+++ git_settings.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+  echo "Set git config"
+  git config --local core.autocrlf input
+  git config --local user.name "Babji8800"
+  git config --local user.email "sviatoslav701@gmail.com"
+  git config user.name
+  git config user.email
